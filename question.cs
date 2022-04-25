d42ef894a94a7515f2eb01f0a26183bd5b9fab12
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-04-25 19:39:44 -0400

    Add files via upload

---
+++ question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class question_21
+    {
+        public string name { get; set; }
+        public string tName { get; set; }
+        public Guid ID { get; set; }
+    }
+
+    public class GenericList<T> where T : question_21
+    {
+        List<question_21> questions = new List<question_21>()
+        {
+            new Customer() { ID = Guid.NewGuid(), name = "Joe", tName = "Smith" },
+            new Customer() { ID = Guid.NewGuid(), name = "Joe", tName = "Smith" },
+
+        
+         foreach (var que in questions.OrderByDescending())
+            {
+                   console.WriteLine(que);
+            
+            }
+        };
+     }
+}
